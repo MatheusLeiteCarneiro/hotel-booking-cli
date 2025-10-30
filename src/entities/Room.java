4ac180b79a77da3a5b8be65c40753d43d520e335
MatheusLeiteCarneiro <matheus.leite.carneiro@gmail.com>
2025-10-30 15:44:57 -0300

    Create Room class

--- src/entities/Room.java
+++ src/entities/Room.java
@@ -1,4 +1,26 @@
 package entities;
 
+import entities.enums.RoomType;
+
 public class Room {
+    private Integer number;
+    private RoomType type;
+
+    public Room() {
+    }
+
+    public Room(Integer number, RoomType type) {
+        this.number = number;
+        this.type = type;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public RoomType getType() {
+        return type;
+    }
+
+
 }
