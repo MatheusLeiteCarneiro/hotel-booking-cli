0b58437b94ecc1db71a9263aebda2c2547cbafbf
MatheusLeiteCarneiro <matheus.leite.carneiro@gmail.com>
2025-10-30 15:51:07 -0300

    Create guest class

--- src/entities/Guest.java
+++ src/entities/Guest.java
@@ -1,4 +1,22 @@
 package entities;
 
 public class Guest {
+    private String fullName;
+    private String email;
+
+    public Guest() {
+    }
+
+    public Guest(String fullName, String email) {
+        this.fullName = fullName;
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
