1308ac3537855f550f1e5d1e223cbb8139e29180
MatheusLeiteCarneiro <matheus.leite.carneiro@gmail.com>
2025-10-30 17:09:11 -0300

    Create Hotel class

--- src/entities/Hotel.java
+++ src/entities/Hotel.java
@@ -1,4 +1,36 @@
 package entities;
 
+import entities.enums.RoomType;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hotel {
+    private List<Room> rooms = new ArrayList<>();
+    private List<Reserve> reserves = new ArrayList<>();
+
+    public Hotel() {
+        loadRooms();
+    }
+    public void loadRooms(){
+        for (int i = 0; i <= 10; i++){
+            rooms.add(new Room((100+i), RoomType.STANDART));
+        }
+        for (int i = 0; i <= 10; i++){
+            rooms.add(new Room((200+i), RoomType.FAMILY));
+        }
+        for (int i = 0; i <= 10; i++){
+            rooms.add(new Room((300+i), RoomType.DELUXE));
+        }
+
+    }
+    public void addReserve(Reserve newReserve){
+        reserves.add(newReserve);
+    }
+
+    public void removeReserve(Reserve newReserve){
+        reserves.remove(newReserve);
+    }
+
+
 }
