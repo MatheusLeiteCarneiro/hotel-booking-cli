46623c0fd05d7dbf93172452c2966a1a8a4eb80f
MatheusLeiteCarneiro <matheus.leite.carneiro@gmail.com>
2025-10-31 21:51:03 -0300

    Feat: verify checkIn and checkOut values /get nights method/ toString method /constant checkIn and checkOut hour and minutes

--- src/entities/Reserve.java
+++ src/entities/Reserve.java
@@ -1,12 +1,16 @@
 package entities;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 
 public class Reserve {
+    private final int checkInHour = 14;
+    private final int checkOutHour = 12;
+    private final int checkInAndCheckOutMinutes = 0;
+
     private Guest guest;
-    private  Room room;
+    private Room room;
     private LocalDate checkInDate;
     private LocalDate checkOutDate;
     private Integer addedPeople;
@@ -15,27 +19,16 @@ public class Reserve {
     public Reserve() {
     }
 
-    public Reserve(Guest guest, Room room, LocalDate checkInDate, LocalDate checkOutDate) {
+    public Reserve(Guest guest) {
         this.guest = guest;
-        this.room = room;
-        this.checkInDate = checkInDate;
-        this.checkOutDate = checkOutDate;
+
     }
-    public Reserve(Guest guest, Room room, LocalDate checkInDate, LocalDate checkOutDate,int addedPeople) {
-        this.guest = guest;
-        this.room = room;
-        this.checkInDate = checkInDate;
-        this.checkOutDate = checkOutDate;
-        this.addedPeople = addedPeople;
-    }
+
 
     public Guest getGuest() {
         return guest;
     }
 
-    public void setGuest(Guest guest) {
-        this.guest = guest;
-    }
 
     public Room getRoom() {
         return room;
@@ -45,22 +38,94 @@ public class Reserve {
         this.room = room;
     }
 
-    public LocalDateTime getCheckInDateTime() {
-        return checkInDate.atTime(14, 00);
+    public int getCheckInHour() {
+        return checkInHour;
+    }
+
+    public int getCheckOutHour() {
+        return checkOutHour;
+    }
+
+    public int getCheckInAndCheckOutMinutes() {
+        return checkInAndCheckOutMinutes;
+    }
+
+    public void setCheckInDate(LocalDate checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public void setCheckOutDate(LocalDate checkOutDate) {
+        this.checkOutDate = checkOutDate;
     }
 
 
+    public LocalDate getCheckInDate() {
+        return checkInDate;
+    }
 
-    public LocalDateTime getCheckOutDateTime() {
-        return checkOutDate.atTime(12, 00);
+    public LocalDate getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public Integer getAddedPeople() {
+        return addedPeople;
+    }
+
+    public void setAddedPeople(Integer addedPeople) {
+        this.addedPeople = addedPeople;
+    }
+
+    public boolean verifyCheckIn(LocalDate checkIn) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        if (checkIn.isBefore(LocalDate.now())) {
+            System.out.println("You can't enter a date before: " + LocalDate.now().format(formatter));
+            System.out.print("Type a new date (DD/MM/YYYY):");
+            return false;
+        }
+        checkInDate = checkIn;
+        return true;
+    }
+
+    public boolean verifyCheckOut(LocalDate checkOut) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        if (checkInDate.isBefore(checkOut)) {
+            checkOutDate = checkOut;
+            return true;
+        }
+        System.out.println("You can only enter a date after the checkIn at: " + checkInDate.format(formatter));
+        System.out.print("Type a new date (DD/MM/YYYY):");
+        return false;
+
+    }
+
+    public int getNights() {
+        return (int) ChronoUnit.DAYS.between(checkInDate, checkOutDate);
     }
 
 
-
-
-    public double totalValue(){
-        int days = (int) ChronoUnit.DAYS.between(checkInDate, checkOutDate);
-        double total = days * room.getType().finalDailyPrice(addedPeople);
+    public double totalValue() {
+        double total = getNights() * room.getType().finalDailyPrice(addedPeople);
         return total;
+    }
+
+    @Override
+    public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Room type: " + room.getType().getName() + "\n");
+        stringBuilder.append("Room number: " + room.getNumber() + "\n");
+        if (addedPeople > 0) {
+            stringBuilder.append("Added person(s): " + addedPeople + "\n");
+        } else {
+            stringBuilder.append("No aditional person\n");
+        }
+        stringBuilder.append("Night(s): " + getNights() + "\n");
+        stringBuilder.append("CheckIn date and time: " + checkInDate.atTime(checkInHour, checkInAndCheckOutMinutes).format(formatter) + "\n");
+        stringBuilder.append("CheckOut date and time: " + checkOutDate.atTime(checkOutHour, checkInAndCheckOutMinutes).format(formatter) + "\n");
+        stringBuilder.append("Price: $" + String.format("%.2f", totalValue()) + "\n");
+
+
+        return stringBuilder.toString();
+
     }
 }
